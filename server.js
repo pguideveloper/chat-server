049b2b5831f2b803ba48330b763911cf6c8b9ea8
Pedro Henrique Guimarães <pedroguimaraes.contato@gmail.com>
2019-03-03 10:54:33 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express       = require('express')
+const cors          = require('cors')
+
+const app           = express() 
+const server        = require('http').createServer(app)
+const io            = require('socket.io')(server)
+let messages        = []
+let connectedUsers  = []
+
+app.use(cors())
+
+io.on('connection', socket => {
+    console.log(`Connected id: ${socket.id}`)
+    socket.on('createUser', data => {
+        console.log(data)
+        connectedUsers.push(data)
+        io.emit('loggedUsers', connectedUsers)
+    })
+    socket.emit('isLoggedIn', socket.id);
+    socket.on('sendMessage', data => {
+        console.log(data)
+        messages.push(data)
+        io.emit('receivedMessage', messages)
+    })
+})
+
+let port = process.env.PORT || 3333 
+server.listen(port)
